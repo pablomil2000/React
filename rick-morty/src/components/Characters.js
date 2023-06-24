09e7ee988f6577cebbb7c0d0d04c89719a3691ac
pablomil2000 <81068583+pablomil2000@users.noreply.github.com>
2023-06-24 11:38:07 +0200

    correccion layout

--- rick-morty/src/components/Characters.js
+++ rick-morty/src/components/Characters.js
@@ -1,51 +1,55 @@
+import React from "react";
+
 export default function Characters(props) {
     const { characters, setCharacters } = props;
 
-
     const resetCharacters = () => {
         setCharacters(null);
-    }
+    };
+
 
     return (
         <div className="characters">
-            <h1>Pesonajes</h1>
-            <span onClick={resetCharacters} className="back-home">Volver al home</span>
-            <div className="container-characters"></div>
-            {characters.map((character, index) => (
-                <div className="character-container" key={index}>
-                    <div>
-                        <img src={character.image} alt={characters.name} />
-                    </div>
-                    <div>
-                        <h3>{character.name}</h3>
-                        <h6>
-                            {character.status === "Alive" ?
-                                (
+            <h1>Personajes</h1>
+            <span className="back-home" onClick={resetCharacters}>
+                Volver a la home
+            </span>
+            <div className="container-characters">
+                {characters.map((character, index) => (
+                    <div className="character-container" key={index}>
+                        <div>
+                            <img src={character.image} alt={character.name} />
+                        </div>
+                        <div>
+                            <h3>{character.name}</h3>
+                            <h6>
+                                {character.status === "Alive" ? (
                                     <>
-                                        <span className="alive">
-                                            Alive
-                                        </span>
+                                        <span className="alive" />
+                                        Alive
                                     </>
-                                ) :
-                                (
+                                ) : (
                                     <>
-                                        <span className="dead">
-                                            Dead
-                                        </span>
+                                        <span className="dead" />
+                                        Dead
                                     </>
                                 )}
-                        </h6>
-                        <p>
-                            <span className="text-grey">Episodios:</span>
-                            <span>{character.episode.length}</span>
-                        </p>
-                        <p className="text-grey">Especie</p>
-                        <span>{character.species}</span>
+                            </h6>
+                            <p>
+                                <span className="text-grey">Episodios: </span>
+                                <span>{character.episode.length}</span>
+                            </p>
+                            <p>
+                                <span className="text-grey">Especie: </span>
+                                <span>{character.species}</span>
+                            </p>
+                        </div>
                     </div>
-                    span
-                </div>
-            ))}
-            <span onClick={resetCharacters} className="back-home">Volver al home</span>
+                ))}
+            </div>
+            <span className="back-home" onClick={resetCharacters}>
+                Volver a la home
+            </span>
         </div>
-    )
+    );
 }
