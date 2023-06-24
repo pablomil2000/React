57c1dde6099a3738d4c9c7c5fd7d447617e42d5d
pablomil2000 <81068583+pablomil2000@users.noreply.github.com>
2023-06-24 09:40:43 +0200

    peticiones Api
    peticiones a la pai de rick y morty

--- rick-morty/src/App.js
+++ rick-morty/src/App.js
@@ -1,12 +1,21 @@
 import imageRickMorty from './img/rick-morty.png'
 import './App.css';
 
+import { useState } from "react";
+
 function App() {
 
-  const reqApi = () => {
-    console.log('click');
+  const [characters, setCharacters] = useState(null);
+
+  const reqApi = async () => {
+
+    const api = await fetch('https://rickandmortyapi.com/api/character');
+    const characterApi = await api.json();
+    setCharacters(characterApi.results);
+
   };
 
+  console.log(characters);
   return (
     <div className="App">
       <header className="App-header">
