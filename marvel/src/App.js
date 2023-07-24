1d812039b2e969d01fec86162e3e08754f3a9b0a
pablomil2000 <81068583+pablomil2000@users.noreply.github.com>
2023-07-24 08:00:53 +0200

    instalacion de paquetes

--- marvel/src/App.js
+++ marvel/src/App.js
@@ -1,10 +1,17 @@
-
 import './App.css';
+import "semantic-ui-css/semantic.min.css";
+import { Button, Icon } from 'semantic-ui-react';
+
 
 function App() {
   return (
     <div>
-      aplicacion de marvel
+      <Button animated>
+        <Button.Content visible>Next</Button.Content>
+        <Button.Content hidden>
+          <Icon name='arrow right' />
+        </Button.Content>
+      </Button>
     </div>
   );
 }
