f7bcd9a943f384ebe68d9d33b449acc4e4c38289
pablomil2000 <81068583+pablomil2000@users.noreply.github.com>
2023-08-01 07:45:02 +0200

    introduccion

--- formulario/src/App.js
+++ formulario/src/App.js
@@ -1,11 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBarMenu from './Components/NavBarMenu'
-
+import Introduction from './Components/Introduction'
 
 function App() {
   return (
     <div>
       <NavBarMenu />
+      <Introduction />
     </div>
   );
 }
