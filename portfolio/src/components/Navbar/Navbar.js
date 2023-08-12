92f0de62ef33cfc028fe7f6bd4c6ebcfa7721497
pablomil2000 <81068583+pablomil2000@users.noreply.github.com>
2023-08-12 08:03:31 +0200

    colores globales

---
+++ portfolio/src/components/Navbar/Navbar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import * as S from './Navbar.styles'
+const Navbar = () => {
+    return <S.ContainerNavBar>ased</S.ContainerNavBar>
+}
+
+export default Navbar
---
+++ portfolio/src/components/Navbar/Navbar.styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { colors } from "../../styles/colors";
+
+export const ContainerNavBar = styled.div`
+height:100px;
+background-color:${colors.TEXT_GREY_NEUTRAL};
+`
