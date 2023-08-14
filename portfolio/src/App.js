297d06f517bc3004aeb6aeaa245824e304c8f01a
pablomil2000 <81068583+pablomil2000@users.noreply.github.com>
2023-08-14 18:09:49 +0200

    vista de proyectos

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Container from './components/Container';
 import Navbar from './components/Navbar';
 import AboutMe from './components/AboutMe';
+import Projects from './components/Projects';
+
 function App() {
   return (
     <>
       <Navbar />
       <Container>
         <AboutMe />
+        <Projects />
       </Container>
     </>
   );
